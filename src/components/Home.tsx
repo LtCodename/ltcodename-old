4e29e0bd2d2d39fec3363b52f81cd9a49d3ff473
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-03-18 18:35:29 -0700

    Unwrapping instead of routs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,31 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 import { Col, Row } from "./Layout";
-import { NavLink } from "react-router-dom";
+import TabGeneral from "./TabGeneral";
+import TabAbout from "./TabAbout";
+import TabExperience from "./TabExperience";
+import TabSkills from "./TabSkills";
+import TabProjects from "./TabProjects";
 
 const LinksGrid = styled(Col)`
     display: flex;
 `;
 
-const Link = styled(NavLink)`
-    text-decoration: none;
-    transition: border-width .5s;
-    height: 20vh;
-    transition: all .5s;
-    font-size: 45px;
-    cursor: pointer;
-	:hover {
-	    text-decoration: none;
-	    height: calc(20vh + 20px);
-	    margin-top: -10px;
-        margin-bottom: -10px;
-        font-size: 50px;
-	}
-	
-	:last-child:hover {
-	    height: calc(20vh + 10px);
-	    margin-bottom: 0;
-	}
+const Link = styled.div`
 `;
 
 const GeneralLink = styled(Link)`
@@ -54,43 +40,132 @@ const ProjectsLink = styled(Link)`
 `;
 
 const TitleWrapper = styled(Row)`
+    text-decoration: none;
+    cursor: pointer;
     justify-content: center;
     align-items: center;
     height: 100%;
+    height: 20vh;
+    transition: all .5s;
+    :hover {
+	    text-decoration: none;
+	    height: calc(20vh + 20px);
+	    margin-top: -10px;
+        margin-bottom: -10px;
+        button {
+            font-size: 50px;
+        }
+	}
+	
+	div:last-child > &:hover {
+	    height: calc(20vh + 10px);
+	    margin-bottom: 0;
+	}
 `;
 
-const Title = styled.span`
+const ToggleButton = styled.button`
     text-transform: uppercase;
     font-weight: 800;
+    font-size: 45px;
+    transition: all .5s;
+    width: 100%;
+    height: 20vh;
+    background: none;
+    border: none;
+    outline: none;
+    color: inherit;
+    cursor: pointer;
+    &:hover,
+    &:focus {
+        outline: none;
+    }
+`;
+
+const ContentWrapper = styled.div<{ opened: boolean }>`
+    height: ${props =>  (props.opened ? 'auto' : 0) };
+    overflow: hidden;
 `;
 
 const Home = () => {
+
+    const [selectedSection, setSelectedSection] = useState('');
+
+    const onToggle = (event:React.MouseEvent) => {
+        //@ts-ignore
+        const { id } = event.target;
+        if (selectedSection !== id) {
+            setSelectedSection(id);
+        } else {
+            setSelectedSection('');
+        }
+    };
+
     return (
         <LinksGrid>
-            <GeneralLink to={`/general`}>
+            <GeneralLink>
                 <TitleWrapper>
-                    <Title>General</Title>
+                    <ToggleButton
+                        type={'button'}
+                        id={'general'}
+                        onClick={onToggle}>
+                        General
+                    </ToggleButton>
                 </TitleWrapper>
+                <ContentWrapper opened={selectedSection === 'general'}>
+                    <TabGeneral/>
+                </ContentWrapper>
             </GeneralLink>
-            <AboutLink to={`/about`}>
+            <AboutLink>
                 <TitleWrapper>
-                    <Title>About</Title>
+                    <ToggleButton
+                        type={'button'}
+                        id={'about'}
+                        onClick={onToggle}>
+                        About
+                    </ToggleButton>
                 </TitleWrapper>
+                <ContentWrapper opened={selectedSection === 'about'}>
+                    <TabAbout/>
+                </ContentWrapper>
             </AboutLink>
-            <ExperienceLink to={`/experience`}>
+            <ExperienceLink>
                 <TitleWrapper>
-                    <Title>Experience</Title>
+                    <ToggleButton
+                        type={'button'}
+                        id={'experience'}
+                        onClick={onToggle}>
+                        Experience
+                    </ToggleButton>
                 </TitleWrapper>
+                <ContentWrapper opened={selectedSection === 'experience'}>
+                    <TabExperience/>
+                </ContentWrapper>
             </ExperienceLink>
-            <SkillsLink to={`/skills`}>
+            <SkillsLink>
                 <TitleWrapper>
-                    <Title>Skills</Title>
+                    <ToggleButton
+                        type={'button'}
+                        id={'skills'}
+                        onClick={onToggle}>
+                        Skills
+                    </ToggleButton>
                 </TitleWrapper>
+                <ContentWrapper opened={selectedSection === 'skills'}>
+                    <TabSkills/>
+                </ContentWrapper>
             </SkillsLink>
-            <ProjectsLink to={`/projects`}>
+            <ProjectsLink>
                 <TitleWrapper>
-                    <Title>Projects</Title>
+                    <ToggleButton
+                        type={'button'}
+                        id={'projects'}
+                        onClick={onToggle}>
+                        Projects
+                    </ToggleButton>
                 </TitleWrapper>
+                <ContentWrapper opened={selectedSection === 'projects'}>
+                    <TabProjects/>
+                </ContentWrapper>
             </ProjectsLink>
         </LinksGrid>
     )
--- src/components/TabAbout.tsx
+++ src/components/TabAbout.tsx
@@ -1,43 +1,10 @@
 import React from 'react';
 import styled from "styled-components";
-import { Col, Row } from "./Layout";
-import { NavLink } from "react-router-dom";
-
-const UltimateWrapper = styled(Col)`
-    color: #990b35;
-    padding-bottom: 20px;
-`;
-
-const Link = styled(NavLink)`
-    text-decoration: none;
-    transition: border-width .5s;
-    height: 20vh;
-    transition: all .5s;
-    font-size: 45px;
-    cursor: pointer;
-    color: #990b35;
-    background: #FFF8E8;
-    margin-bottom: 30px;
-	:hover {
-	    text-decoration: none;
-        font-size: 50px;
-	}
-`;
-
-const TitleWrapper = styled(Row)`
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-`;
-
-const Title = styled.span`
-    text-transform: uppercase;
-    font-weight: 800;
-`;
+import { Col } from "./Layout";
 
 const GeneralColumn = styled(Col)`
     align-items: center;
-    padding: 0 20px;
+    padding: 0 0 30px 0;
 `;
 
 const TextWrapper = styled(Col)`
@@ -56,14 +23,9 @@ const Paragraph = styled.span`
     margin-bottom: 10px;
 `;
 
-const SmallNavLink = styled(NavLink)`
+const SmallNavLink = styled.span`
     font-weight: 800;
     color: #990b35;
-    text-decoration: none;
-	:hover {
-	    color: #990b35;
-	    text-decoration: underline;
-	}
 `;
 
 const TextLink = styled.a`
@@ -78,28 +40,21 @@ const TextLink = styled.a`
 
 const TabAbout = () => {
     return (
-        <UltimateWrapper>
-            <Link to={`/home`}>
-                <TitleWrapper>
-                    <Title>About</Title>
-                </TitleWrapper>
-            </Link>
-            <GeneralColumn>
-                <TextWrapper>
-                    <ParagraphTitle>Introduction</ParagraphTitle>
-                    <Paragraph>Open for Front End Developer and/or Project Manager positions.</Paragraph>
-                    <Paragraph>I've been working in game development for almost 5 years as a Community Manager, UI Programmer
-                        and then Project Manager. Now I'm making my first steps as a Front-End Developer. Check out my <SmallNavLink to={'/projects'}>Projects</SmallNavLink> section
-                        and my <TextLink target="_blank" href="https://github.com/LtCodename">GitHub</TextLink> profile to take a look at the stuff I’ve created. Programming now is an integral part
-                        of my life, I enjoy watching how things on a screen evolve from a first line of simple code to a vast
-                        and complex projects with a ton of secret ingredients under the hood.</Paragraph>
-                    <Paragraph>As much as I love making software I fond of managing the process of making as well. I love keeping things in order. During my time in game development I've enjoyed so much taking care of my team, extinguishing conflicts, making sure everybody doing exactly what they need to do, that they have all the material resources they need. There is something in it watching your team working like a clockwork. I'm proficient with writing documentation, working in bug trackers and planning software.</Paragraph>
-                    <Paragraph>If you have a job for me - contact me anytime in the most suitable for you way. All my contact info is in <SmallNavLink to={'/general'}>General</SmallNavLink> section.</Paragraph>
-                    <ParagraphTitle>Education</ParagraphTitle>
-                    <Paragraph>I have Master Of Science Degree in Management Information Systems that I've obtained in Kyiv National Economic University in 2010-2015.</Paragraph>
-                </TextWrapper>
-            </GeneralColumn>
-        </UltimateWrapper>
+        <GeneralColumn>
+            <TextWrapper>
+                <ParagraphTitle>Introduction</ParagraphTitle>
+                <Paragraph>Open for Front End Developer and/or Project Manager positions.</Paragraph>
+                <Paragraph>I've been working in game development for almost 5 years as a Community Manager, UI Programmer
+                    and then Project Manager. Now I'm making my first steps as a Front-End Developer. Check out my <SmallNavLink>Projects</SmallNavLink> section
+                    and my <TextLink target="_blank" href="https://github.com/LtCodename">GitHub</TextLink> profile to take a look at the stuff I’ve created. Programming now is an integral part
+                    of my life, I enjoy watching how things on a screen evolve from a first line of simple code to a vast
+                    and complex projects with a ton of secret ingredients under the hood.</Paragraph>
+                <Paragraph>As much as I love making software I fond of managing the process of making as well. I love keeping things in order. During my time in game development I've enjoyed so much taking care of my team, extinguishing conflicts, making sure everybody doing exactly what they need to do, that they have all the material resources they need. There is something in it watching your team working like a clockwork. I'm proficient with writing documentation, working in bug trackers and planning software.</Paragraph>
+                <Paragraph>If you have a job for me - contact me anytime in the most suitable for you way. All my contact info is in <SmallNavLink>General</SmallNavLink> section.</Paragraph>
+                <ParagraphTitle>Education</ParagraphTitle>
+                <Paragraph>I have Master Of Science Degree in Management Information Systems that I've obtained in Kyiv National Economic University in 2010-2015.</Paragraph>
+            </TextWrapper>
+        </GeneralColumn>
     )
 };
 
