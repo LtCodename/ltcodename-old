7f62a985835f5a6dfdd763c472ea281e5bd87807
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-03-25 17:49:33 -0700

    Route system removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 import Home from "./components/Home";
-import TabGeneral from "./components/TabGeneral";
-import TabAbout from "./components/TabAbout";
-import TabExperience from "./components/TabExperience";
-import TabSkills from "./components/TabSkills";
-import TabProjects from "./components/TabProjects";
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -21,23 +15,10 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 const App: React.FC = () => {
-    const allContent = (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/home" component={Home}/>
-                <Route exact path="/general" component={TabGeneral}/>
-                <Route exact path="/about" component={TabAbout}/>
-                <Route exact path="/experience" component={TabExperience}/>
-                <Route exact path="/skills" component={TabSkills}/>
-                <Route exact path="/projects" component={TabProjects}/>
-                <Redirect to="/home"/>
-            </Switch>
-        </BrowserRouter>
-    );
 
     return (
         <>
-            {allContent}
+            <Home/>
             <GlobalStyles/>
         </>
     );
