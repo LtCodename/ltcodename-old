206e1c28f5c985587ba84adafb5fb9a45be95e5d
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-05-15 09:13:28 -0700

    Minor CSS and text changes.

--- src/components/TabProjects.tsx
+++ src/components/TabProjects.tsx
@@ -14,7 +14,7 @@ const ProjectPiece = styled(Col)`
 
 const ProjectName = styled.span`
     font-weight: 800;
-    font-size: 25px;
+    font-size: 19px;
     margin-bottom: 10px;
 `;
 
@@ -47,6 +47,7 @@ const TabProjects = () => {
                 <DescriptionWrapper>
                     <Paragraph>I am a gamer and I needed a way to track what games I've completed, what I wish to play later, what is an absolute garbage and what I want to try out with friends. Also I am a data freak so I needed to see my gaming history for previous years. There are some platforms for this purpose out there but I needed this system to be fully customizable and flex. So I've made one myself.</Paragraph>
                     <Paragraph>It's stable and usable but super raw design-wise and still lacking some features I've planned initially. It's in alpha state and in active development though and I'd appreciate all feedback I can get. Wanna help, propose features or just chat about what you are playing right now? Contact me!</Paragraph>
+                    <Paragraph>It's done using React, Firebase, RAWG API.</Paragraph>
                 </DescriptionWrapper>
                 <Links>
                     <ProjectLink
@@ -70,37 +71,10 @@ const TabProjects = () => {
                 </Links>
             </ProjectPiece>
             <ProjectPiece>
-                <ProjectName>Grid</ProjectName>
-                <DescriptionWrapper>
-                    <Paragraph>Formula 1 is one of my hobbies these days and, again, I need to track all the various pieces of data this sport produces during the season: Grand Prix results, personal driver statistics, standings and things like that. At first I was using regular spreedsheets but hey I'm a programmer, I can make a proper platform for it and infinitely customise it how I want.</Paragraph>
-                    <Paragraph>Like Game Keeper, the design side is not a forte for this project but I'm trying to keep it clean and simple. Go ahead download the code and poke at it if you must.</Paragraph>
-                </DescriptionWrapper>
-                <Links>
-                    <ProjectLink
-                        target="_blank"
-                        href="https://grid.ltcodename.com/">
-                        <SVG aria-hidden="true" focusable="false" data-prefix="fas" data-icon="link"
-                             role="img"
-                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                            <path d="M326.612 185.391c59.747 59.809 58.927 155.698.36 214.59-.11.12-.24.25-.36.37l-67.2 67.2c-59.27 59.27-155.699 59.262-214.96 0-59.27-59.26-59.27-155.7 0-214.96l37.106-37.106c9.84-9.84 26.786-3.3 27.294 10.606.648 17.722 3.826 35.527 9.69 52.721 1.986 5.822.567 12.262-3.783 16.612l-13.087 13.087c-28.026 28.026-28.905 73.66-1.155 101.96 28.024 28.579 74.086 28.749 102.325.51l67.2-67.19c28.191-28.191 28.073-73.757 0-101.83-3.701-3.694-7.429-6.564-10.341-8.569a16.037 16.037 0 0 1-6.947-12.606c-.396-10.567 3.348-21.456 11.698-29.806l21.054-21.055c5.521-5.521 14.182-6.199 20.584-1.731a152.482 152.482 0 0 1 20.522 17.197zM467.547 44.449c-59.261-59.262-155.69-59.27-214.96 0l-67.2 67.2c-.12.12-.25.25-.36.37-58.566 58.892-59.387 154.781.36 214.59a152.454 152.454 0 0 0 20.521 17.196c6.402 4.468 15.064 3.789 20.584-1.731l21.054-21.055c8.35-8.35 12.094-19.239 11.698-29.806a16.037 16.037 0 0 0-6.947-12.606c-2.912-2.005-6.64-4.875-10.341-8.569-28.073-28.073-28.191-73.639 0-101.83l67.2-67.19c28.239-28.239 74.3-28.069 102.325.51 27.75 28.3 26.872 73.934-1.155 101.96l-13.087 13.087c-4.35 4.35-5.769 10.79-3.783 16.612 5.864 17.194 9.042 34.999 9.69 52.721.509 13.906 17.454 20.446 27.294 10.606l37.106-37.106c59.271-59.259 59.271-155.699.001-214.959z"/>
-                        </SVG>
-                    </ProjectLink>
-                    <ProjectLink
-                        target="_blank"
-                        href="https://github.com/LtCodename/grid">
-                        <SVG aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
-                             role="img" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 496 512">
-                            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
-                        </SVG>
-                    </ProjectLink>
-                </Links>
-            </ProjectPiece>
-            <ProjectPiece>
                 <ProjectName>Watcher</ProjectName>
                 <DescriptionWrapper>
                     <Paragraph>Okay, we've covered games and Formula 1 so now it's movies time! Again I'm tracking things: what my favorite directors are, how many of their movies I have watched, what their movies are currently in production, what is in the theaters now that I want to watch and so on. This will be a huge one data-wise as soon as it is finished.</Paragraph>
-                    <Paragraph>I'm using TypeScript, React, Firebase and OMDb API for real movie data. It's not hardcoded.</Paragraph>
+                    <Paragraph>It's done using React, TypeScript, Firebase and OMDb API. I'm about to redo it with Angular and Angular Material.</Paragraph>
                 </DescriptionWrapper>
                 <Links>
                     <ProjectLink
@@ -115,6 +89,34 @@ const TabProjects = () => {
                     <ProjectLink
                         target="_blank"
                         href="https://github.com/LtCodename/Watcher">
+                        <SVG aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
+                             role="img" xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 496 512">
+                            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
+                        </SVG>
+                    </ProjectLink>
+                </Links>
+            </ProjectPiece>
+            <ProjectPiece>
+                <ProjectName>Grid</ProjectName>
+                <DescriptionWrapper>
+                    <Paragraph>Formula 1 is one of my hobbies these days and, again, I need to track all the various pieces of data this sport produces during the season: Grand Prix results, personal driver statistics, standings and things like that. At first I was using regular spreedsheets but hey I'm a programmer, I can make a proper platform for it and infinitely customise it how I want.</Paragraph>
+                    <Paragraph>Like Game Keeper, the design side is not a forte for this project but I'm trying to keep it clean and simple. Go ahead download the code and poke at it if you must.</Paragraph>
+                    <Paragraph>It's done using React, StyledComponents and Firebase.</Paragraph>
+                </DescriptionWrapper>
+                <Links>
+                    <ProjectLink
+                        target="_blank"
+                        href="https://grid.ltcodename.com/">
+                        <SVG aria-hidden="true" focusable="false" data-prefix="fas" data-icon="link"
+                             role="img"
+                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M326.612 185.391c59.747 59.809 58.927 155.698.36 214.59-.11.12-.24.25-.36.37l-67.2 67.2c-59.27 59.27-155.699 59.262-214.96 0-59.27-59.26-59.27-155.7 0-214.96l37.106-37.106c9.84-9.84 26.786-3.3 27.294 10.606.648 17.722 3.826 35.527 9.69 52.721 1.986 5.822.567 12.262-3.783 16.612l-13.087 13.087c-28.026 28.026-28.905 73.66-1.155 101.96 28.024 28.579 74.086 28.749 102.325.51l67.2-67.19c28.191-28.191 28.073-73.757 0-101.83-3.701-3.694-7.429-6.564-10.341-8.569a16.037 16.037 0 0 1-6.947-12.606c-.396-10.567 3.348-21.456 11.698-29.806l21.054-21.055c5.521-5.521 14.182-6.199 20.584-1.731a152.482 152.482 0 0 1 20.522 17.197zM467.547 44.449c-59.261-59.262-155.69-59.27-214.96 0l-67.2 67.2c-.12.12-.25.25-.36.37-58.566 58.892-59.387 154.781.36 214.59a152.454 152.454 0 0 0 20.521 17.196c6.402 4.468 15.064 3.789 20.584-1.731l21.054-21.055c8.35-8.35 12.094-19.239 11.698-29.806a16.037 16.037 0 0 0-6.947-12.606c-2.912-2.005-6.64-4.875-10.341-8.569-28.073-28.073-28.191-73.639 0-101.83l67.2-67.19c28.239-28.239 74.3-28.069 102.325.51 27.75 28.3 26.872 73.934-1.155 101.96l-13.087 13.087c-4.35 4.35-5.769 10.79-3.783 16.612 5.864 17.194 9.042 34.999 9.69 52.721.509 13.906 17.454 20.446 27.294 10.606l37.106-37.106c59.271-59.259 59.271-155.699.001-214.959z"/>
+                        </SVG>
+                    </ProjectLink>
+                    <ProjectLink
+                        target="_blank"
+                        href="https://github.com/LtCodename/grid">
                         <SVG aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
                              role="img" xmlns="http://www.w3.org/2000/svg"
                              viewBox="0 0 496 512">
